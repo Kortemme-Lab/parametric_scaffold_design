056449f9b89b253d195bfd43f8d921ae0e6b1fb8
xingjiepan <xingjiepan@gmail.com>
2017-07-21 16:01:28 -0700

    Add some basic files.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # parametric_scaffold_design
 PyRosetta scripts for parametric protein scaffold design
+
+## Dependents
+python2.7
+PyRosetta
---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2
+
+from distutils.core import setup
+
+setup(
+    name='parametric_protein_scaffold_design',
+    version='0.0.0',
+    author='Xingjie Pan',
+    author_email='xingjiepan@gmail.com',
+    url='https://github.com/Kortemme-Lab/parametric_scaffold_design',
+    packages=[
+        'parametric_protein_scaffold_design',
+    ],
+    install_requires=[
+        'matplotlib',
+    ],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+    description='PyRosetta scripts for parametric protein scaffold design',
+    long_description=open('README.md').read(),
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Intended Audience :: Science/Research',
+    ],
+)
